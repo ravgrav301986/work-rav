be8d100d6b26bd3996850660660960000564c2ab
Ravcoder <dzhambulat.kagermanov@gmail.com>
2025-05-28 23:04:57 +0300

    New commit

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,21 +4,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://ravcoder.ru',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
+      priority: 1.0,
     },
-    {
-      url: 'https://ravcoder.ru/about',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://ravcoder.ru/blog',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
+   
   ]
 }
