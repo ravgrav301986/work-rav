5ccea9d3cdfaad5ef34b265b63ed8dd08ad13edc
Ravcoder <dzhambulat.kagermanov@gmail.com>
2025-07-02 01:24:55 +0300

    New commit

--- app/(main)/services/page.tsx
+++ app/(main)/services/page.tsx
@@ -1,9 +1,5 @@
-
-'use client'
-import { MD_MID } from '@/shared/constants/breakpoints';
-import { useBodyClassName, useScreen } from '@/shared/hooks';
 import { cn } from '@/shared/lib/cn';
-import { UiButton } from '@/shared/ui/UiButton';
+
 import { UiContainer } from '@/shared/ui/UiContainer';
 import { UiDecorationTitle } from '@/shared/ui/UiDecorationTitle';
 import { UiTypography } from '@/shared/ui/UiTypography';
@@ -11,18 +7,18 @@ import { ContentItem } from '@/widgets/HomeAbout/ui/ContentItem';
 
 import Image from 'next/image';
 import React from 'react';
- import type { Metadata } from 'next'
+import  { Metadata } from 'next'
 
-  const metadata: Metadata = {
+ export const metadata: Metadata =   {
 	title: 'Услуги | Rav.Coder',
 	description: 'Ознакомьтесь с полным спектром наших услуг, включая веб-разработку, UI/UX дизайн, техническую поддержку и SEO-продвижение.',
 }
     const Services = () => {
-			const { screenWidth } = useScreen()
+		
 		
      return (
 		<section className='md-mid:py-10 py-7  md-mid:px-7 px-5'>
-	     <UiContainer className={cn('h-full', useBodyClassName)}>
+	     <UiContainer >
 				<UiDecorationTitle className=' md-mid:text-3xl text-2xl'>
 					Услуги
 				</UiDecorationTitle>
@@ -30,54 +26,38 @@ import React from 'react';
 					
 					<UiTypography
 						font='Montserrat-B'
-						className='text-left md-mid:text-[20px] text-1xl text-amber-50'
+						className='text-left md-mid:text-[20px] text-1xl text-amber-50 text-2xl'
 					>
 						Всё для вашего проекта
 						</UiTypography>
 						<br />
-					<UiTypography font='Montserrat-R' className='text-[20px] mt-4 mb-7  text-amber-50'>
-						
-						
+					<UiTypography font='Montserrat-R' className='text-[20px] mt-4 mb-7  text-amber-50 text-2xl'>
 						
 					Комплексные <span className='main-text-gradient'>DIGITAL-РЕШЕНИЯ</span> для роста <br/> вашего бизнеса — от идеи до поддержки.
-						
-					</UiTypography>	
+			    	</UiTypography>	
 					
-					
-						
-					
-						
-					<UiButton theme='fill-invert' className='uppercase w-max'>
-						<UiTypography font='Montserrat-SB'><a href="https://t.me/Ravcoderbot_bot">Заказать проект</a></UiTypography>
-					</UiButton>
 					<Image
 						src={
-							
-							screenWidth >= MD_MID
-								? '/images/Home/Serveces/servis-bg-Frame 6153.png'
-								: '/images/Home/Serveces/servis-bg-Frame 6153.png'
+						'/images/Home/Serveces/servis-bg-Frame 6153.png'
+						
 						}
 						alt='Главный фон'
-						width={screenWidth >= MD_MID ? 1440 : 380}
-						height={screenWidth >= MD_MID ? 450 : 555}
+						width={1400}
+						height={500}
 						className={cn(
-							'absolute inset-0 size-full  widht-avto object-cover -z-[1] md-mid:rounded-md',
-							{
-								'object-top': screenWidth < MD_MID,
-							}
+							'absolute inset-0 size-full  widht-avto object-cover -z-[1] md-mid:rounded-md rounded-lg',
+						
 						)}
 					/>
 				</div>
 			</UiContainer>
-						
-						
-			  <p className='p-20 md-mid:py-15 py-7 md-mid:px-7 px-5 '>Мы разрабатываем современные <span className='main-text-gradient'>сайты, мобильные приложения и цифровые сервисы,</span>  
+			  <p className='p-20  md-mid:py-15 py-7 md-mid:px-7 px-5 text-2xl'>Мы разрабатываем современные <span className='main-text-gradient'>сайты, мобильные приложения и цифровые сервисы,</span>  
 				 которые помогают бизнесу расти, быть заметным и удобным для клиентов. В центре нашей работы — сочетание дизайна, технологий и понимания задач заказчика.</p>
 
 				 <UiDecorationTitle className='md-mid:text-3xl text-2xl'>
 				 Наши ключевые направления:
 				</UiDecorationTitle>
-					<ul className={cn('flex flex-col gap-y-8', useBodyClassName)}>
+					<ul className={cn('flex flex-col gap-y-8 text-2xl')}>
 							<ContentItem>
 							<UiTypography  font='Montserrat-M' tag='span'>
 									Разработка веб-сайтов и приложений
@@ -116,13 +96,13 @@ import React from 'react';
 								— внедряем ИИ-решения для автоматизации, персонализации и улучшения пользовательского опыта.
 								</ContentItem>
 						</ul>
-						  <p className='p-40 md-mid:py-10 py-7 md-mid:px-7 px-5 text-center'><span className='main-text-gradient'>Мы предлагаем гибкий формат сотрудничества: от разовых задач до полного сопровождения проектов.</span>  </p>
+						  <p className='p-40 md-mid:py-10 py-7 md-mid:px-7 px-5 text-center text-2xl'><span className='main-text-gradient'>Мы предлагаем гибкий формат сотрудничества: от разовых задач до полного сопровождения проектов.</span>  </p>
 			
                  <UiDecorationTitle className='md-mid:text-3xl text-2xl'>
 				 Технологии будущего уже сегодня
 				</UiDecorationTitle>
                   
-				<ul className={cn('flex flex-col gap-y-5', useBodyClassName)}>
+				<ul className={('flex flex-col gap-y-5 text-2xl')}>
 			<ContentItem>
 				 Интегрируем решения<UiTypography font='Montserrat-M' tag='span'>
 					 <br/>  на базе нейросетей
